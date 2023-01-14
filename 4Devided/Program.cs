e89a159e7dd0014c503446e3917d1c992d414687
Kate V <katsiarynait@gmail.com>
2023-01-15 02:49:21 +0300

    Разделила с остатком

---
+++ 4Devided/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое целое число");
+String number1 = Console.ReadLine();
+double a = Convert.ToDouble(number1);
+Console.WriteLine("Введите второе целое число");
+String number2 = Console.ReadLine();
+double b = Convert.ToDouble(number2);
+double result = a/b;
+Console.WriteLine(result);
+
