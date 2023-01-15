2332471708c67219d155a732bf7efc3b14391dce
Kate V <katsiarynait@gmail.com>
2023-01-15 03:52:21 +0300

    Задача с гирями более сложное решение (найти ошибки)

---
+++ 6Weights/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int weight = (5,10,7,1,3);
+int n = 5;
+int i = 0;
+int max = weight(i);
+while (i<n)
+{
+  if (weight(i) < weight(i+1))
+  {
+   max = weight(i+1);
+   i++;
+  }  
+}
+Console.WriteLine(max);
+
+Console.WriteLine("Hello, World!");
