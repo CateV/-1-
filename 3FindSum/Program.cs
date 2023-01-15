a454b58ea1c4f712db2ea204283f51b9cff6c82f
Kate V <katsiarynait@gmail.com>
2023-01-15 03:02:58 +0300

    добавила рандомные числа с разным диапазоном в сумму

--- 3FindSum/Program.cs
+++ 3FindSum/Program.cs
@@ -2,7 +2,7 @@
 
 int number1 = new Random().Next(1, 6);
 Console.WriteLine(number1);
-int number2 = new Random().Next(1, 6);
+int number2 = new Random().Next(1, 4);
 Console.WriteLine(number2);
 int result = number1+number2;
 Console.WriteLine(result);
