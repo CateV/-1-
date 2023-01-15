3c03106cc63fa93958d31e01f6bf65f86a30e273
Kate V <katsiarynait@gmail.com>
2023-01-15 03:29:22 +0300

    Обращаемся по-особенному к сыну

---
+++ 5Greetings/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите Ваше имя");
+string name = Console.ReadLine();
+if (name.ToLower() == "деймон")
+{    
+    Console.WriteLine("Привет, сыночек мой любимый Деюшка!");
+}
+else
+{
+    Console.WriteLine($"Привет, {name}!");  
+}
