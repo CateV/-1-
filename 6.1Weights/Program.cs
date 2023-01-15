6f2c3a8a1b9b2dd4188fc64a36b290ad4bb5e7f3
Kate V <katsiarynait@gmail.com>
2023-01-15 03:57:54 +0300

    Задача с гирями (решение преподавателя)

---
+++ 6.1Weights/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 5;
+int b = 2;
+int c = 4;
+int d = 1;
+int e = 9;
+
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.WriteLine($"значение max = {max}");
