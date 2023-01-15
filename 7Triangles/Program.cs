a61d5d0a7b7b6d63c6f1419ba7d9da009696a9d3
Kate V <katsiarynait@gmail.com>
2023-01-15 12:30:45 +0300

    ну хоть что-то заработало в треугольниках

--- 7Triangles/Program.cs
+++ 7Triangles/Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Clear();
-int xa = 40; ya = 1;
-int xb = 1; yb = 30;
-int xc = 60; yc = 30;
+int xa = 30; 
+int ya = 1;
+int xb = 1; 
+int yb = 10;
+int xc = 15; 
+int yc = 15;
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
-
+Console.Write("+");
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
-
+Console.Write("+");
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+Console.Write("+");
