5d97f0ae68160038b41ae1cbaadd29b775feb27f
Kate V <katsiarynait@gmail.com>
2023-01-15 04:30:31 +0300

    треугольники из точек, найти ошибку, пойти дальше

---
+++ 7Triangles/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+int xa = 40; ya = 1;
+int xb = 1; yb = 30;
+int xc = 60; yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
