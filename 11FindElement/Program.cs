9576ca10904f23349d152f1076c68d168df1fe60
Kate V <katsiarynait@gmail.com>
2023-01-15 13:53:48 +0300

    Нашли совпадение числа в массиве и исключили нахождение второго совпадения

---
+++ 11FindElement/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = {15,45,51,59,54,25,55,95};
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+   if (array[index] == find) 
+   {
+    Console.WriteLine(index);
+    }
+   
+    index++;
+}
