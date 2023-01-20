a2d965923040aada0d4f09d61cb86aa730fc8a02
Kate V <katsiarynait@gmail.com>
2023-01-20 03:54:09 +0300

    без ошибок, через массив и функцию

--- 9FindMaxWithMethods/Program.cs
+++ 9FindMaxWithMethods/Program.cs
@@ -1,26 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int Max(int arg1, int arg2, int arg3);
+int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
-    if (arg2>arg1) result = arg2;
-    if (arg3>arg2) result = arg3;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
     return result;
 }
 
-int a1 = 5;
-int a2 = 15;
-int a3 = 45;
-int a4 = 51;
-int a5 = 59;
-int a6 = 54;
-int a7 = 25;
-int a8 = 55;
-int a9 = 95;
+int[] x = { 5, 2, 0, -7, 7, -3, -1, 77, 19 };
 
-int max1 = Max(a1, a2, a3);
+int max = Max(Max(x[0], x[1], x[2]), Max(x[3], x[4], x[5]), Max(x[6], x[7], x[8]));
+
+/*int max1 = Max(a1, a2, a3);
 int max2 = Max(a4, a5, a6);
 int max3 = Max(a7, a8, a9);
-int max = Max(max1, max2, max3);
+int max = Max(max1, max2, max3);*/
 
 // int max = Max(Max(a1, a2, a3), Max(a4, a5, a6), Max(a7, a8, a9));
 
